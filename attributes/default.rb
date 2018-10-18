2917ca58f0c01c9c6cddb7325708bab478f33746
Brian Chau <bchau.gt@gmail.com>
2018-10-17 23:25:33 -0700

    Minor updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['contrast-teamserver']['installer'] = 'contrast-installer.sh'
 default['contrast-teamserver']['license'] = 'prod_dev.lic'
 
 # URL to access TeamServer
-default['contrast-teamserver']['teamserver_url'] = 'http\://192.168.33.100\:8080/Contrast'
+default['contrast-teamserver']['teamserver_url'] = 'http\://192.168.33.100\:8888/Contrast'
 
 # TeamServer's MySQL `innodb_buffer_pool_size`
 default['contrast-teamserver']['innodb_buffer_pool_size'] = '4096M'
